eec8249c4a6c104a13def74507de67be37364980
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-02-23 20:11:44 +0700

    feat: added middleware

---
+++ frontend-next/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    if(request.nextUrl.pathname === '/auth/login'){
+        return NextResponse.next()
+    }
+
+    const token = request.cookies.get('token')
+
+    if (!token) {
+        return NextResponse.redirect(new URL("/auth/login", request.url))
+    }
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
